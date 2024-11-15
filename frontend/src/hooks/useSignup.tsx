d027eab32a947fbacf681df63a8a48ce759006ce
kevin young <you.k2208@gmail.com>
2024-11-14 21:04:37 -0800

    added useLogout hook and functionality and added react-hot-toast for error notification popups

--- frontend/src/hooks/useSignup.tsx
+++ frontend/src/hooks/useSignup.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 type SignupInputs = {
   fullName: string;
@@ -29,6 +30,7 @@ const useSignup = () => {
       setAuthUser(data);
     } catch (error: any) {
       console.error(error.message);
+      toast.error(error.message);
     } finally {
       setLoading(false);
     }
