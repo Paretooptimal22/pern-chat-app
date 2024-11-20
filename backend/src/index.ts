60a1a92304ef6d0e2b4b8b4b7198d4859e8fe334
kevin young <you.k2208@gmail.com>
2024-11-19 21:09:13 -0800

    reconfigured deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import express from "express";
 import cookieParser from "cookie-parser";
 
@@ -9,12 +11,20 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cookieParser()); // for parsing cookies
 app.use(express.json()); // for parsing application/json
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV !== "development") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
